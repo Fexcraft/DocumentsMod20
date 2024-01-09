32c26cc967a042e8ded82270d1232b9be4daf9e1
Ferdinand Calo <fexcalo@gmail.com>
2024-01-10 00:58:36 +0100

    Update Documents.java

--- src/main/java/net/fexcraft/mod/documents/Documents.java
+++ src/main/java/net/fexcraft/mod/documents/Documents.java
@@ -4,12 +4,18 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.logging.LogUtils;
 import net.fexcraft.mod.documents.data.Document;
 import net.fexcraft.mod.documents.data.DocumentItem;
+import net.fexcraft.mod.documents.packet.GuiPacket;
+import net.fexcraft.mod.documents.packet.SyncPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.*;
@@ -30,6 +36,10 @@ import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
+import net.neoforged.neoforge.network.registration.IPayloadRegistrar;
+import net.neoforged.neoforge.network.registration.NetworkRegistry;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -44,12 +54,14 @@ public class Documents {
     public static final Logger LOGGER = LogUtils.getLogger();
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
     public static final DeferredItem<Item> DOCITEM = ITEMS.registerItem("document", func -> new DocumentItem());
+    public static final ResourceLocation SYNC_PACKET = new ResourceLocation(MODID, "sync");
+    public static final ResourceLocation UI_PACKET = new ResourceLocation(MODID, "ui");
 
-    public Documents(IEventBus modEventBus){
-        modEventBus.addListener(this::commonSetup);
-        ITEMS.register(modEventBus);
+    public Documents(IEventBus eventbus){
+        eventbus.addListener(this::commonSetup);
+        ITEMS.register(eventbus);
         NeoForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
+        eventbus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event){
@@ -70,10 +82,20 @@ public class Documents {
     public static class Events {
 
         @SubscribeEvent
+        public static void register(final RegisterPayloadHandlerEvent event) {
+            final IPayloadRegistrar registrar = event.registrar(MODID).versioned("1.0.0").optional();
+            registrar.common(SYNC_PACKET, SyncPacket::read, handler -> handler.client(SyncPacket::handle));
+            registrar.common(UI_PACKET, GuiPacket::read, handler -> {
+                handler.server(GuiPacket::handle_server);
+                handler.client(GuiPacket::handle_client);
+            });
+        }
+
+        @SubscribeEvent
         public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event){
             if(event.getEntity().level().isClientSide) return;
             DocRegistry.opj(event.getEntity());
-            //TODO CHANNEL.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)event.getPlayer()), new SyncPacket(DocRegistry.confmap));
+            PacketDistributor.PLAYER.with((ServerPlayer)event.getEntity()).send(new SyncPacket(DocRegistry.confmap));
         }
 
         @SubscribeEvent
@@ -115,7 +137,7 @@ public class Documents {
                     }
                     DocRegistry.init();
                     entity.getServer().getPlayerList().getPlayers().forEach(player -> {
-                        //TODO CHANNEL.send(PacketDistributor.PLAYER.with(() -> player), new SyncPacket(DocRegistry.confmap));
+            PacketDistributor.PLAYER.with(player).send(new SyncPacket(DocRegistry.confmap));
                     });
                     cmd.getSource().sendSystemMessage(Component.translatable("documents.cmd.docs_reloaded"));
                     return 0;
