bcaa27177b879b998eb66f3450bedbed83f91d42
Ferdinand Calo <fexcalo@gmail.com>
2024-01-10 01:32:11 +0100

    Update DocViewerContainer.java

--- src/main/java/net/fexcraft/mod/documents/gui/DocViewerContainer.java
+++ src/main/java/net/fexcraft/mod/documents/gui/DocViewerContainer.java
@@ -1,74 +1,64 @@
 package net.fexcraft.mod.documents.gui;
 
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.UUID;
-
-import com.mojang.authlib.GameProfile;
-import net.fexcraft.mod.documents.DocPerms;
 import net.fexcraft.mod.documents.DocRegistry;
 import net.fexcraft.mod.documents.Documents;
 import net.fexcraft.mod.documents.data.Document;
-import net.fexcraft.mod.documents.data.FieldData;
-import net.fexcraft.mod.documents.data.FieldType;
-import net.fexcraft.mod.documents.packet.GuiPacket;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.INamedContainerProvider;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.network.PacketBuffer;
-import net.minecraft.util.Hand;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.fml.network.NetworkHooks;
-import net.minecraftforge.fml.network.PacketDistributor;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
 
-public class DocViewerContainer extends Container implements UiPacketReceiver {
+public class DocViewerContainer extends AbstractContainerMenu implements UiPacketReceiver {
 
     protected ItemStack stack;
     protected Document doc;
     protected DocViewerScreen screen;
-    protected PlayerEntity player;
+    protected Player player;
     protected int page = 0;
 
-    public DocViewerContainer(int id, PlayerInventory inv){
+    public DocViewerContainer(int id, Inventory inv){
         super(Documents.DOC_VIEWER.get(), id);
-        stack = inv.player.getItemInHand(Hand.MAIN_HAND);
+        stack = inv.player.getItemInHand(InteractionHand.MAIN_HAND);
         player = inv.player;
         doc = DocRegistry.get(stack);
     }
 
-    public DocViewerContainer(int id, PlayerInventory inv, PacketBuffer buffer){
+    public DocViewerContainer(int id, Inventory inv, FriendlyByteBuf buffer){
         this(id, inv);
         page = buffer.readInt();
     }
 
     @Override
-    public boolean stillValid(PlayerEntity player){
+    public ItemStack quickMoveStack(Player player, int i){
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean stillValid(Player player){
         return true;
     }
 
     @Override
-    public void onPacket(CompoundNBT com, boolean client){
+    public void onPacket(CompoundTag com, boolean client){
         Documents.LOGGER.info(client + " " + com);
         if(com.contains("open_page")){
             if(client) return;
-            NetworkHooks.openGui((ServerPlayerEntity)player, new INamedContainerProvider() {
+            /*NetworkHooks.openGui((ServerPlayer)player, new INamedContainerProvider() {
                 @Override
-                public ITextComponent getDisplayName(){
-                    return new StringTextComponent("Document Viewer");
+                public Component getDisplayName(){
+                    return Component.literal("Document Viewer");
                 }
 
                 @Override
-                public Container createMenu(int i, PlayerInventory pinv, PlayerEntity ent){
+                public AbstractContainerMenu createMenu(int i, Inventory pinv, Player ent){
                     return new DocViewerContainer(i, pinv);
                 }
-            }, buf -> buf.writeInt(com.getInt("open_page")));
+            }, buf -> buf.writeInt(com.getInt("open_page")));*/
             return;
         }
     }
