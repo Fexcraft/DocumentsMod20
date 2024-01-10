0ac7a552a6d221ae4acac0c4cfc7d47389342d37
Ferdinand Calo <fexcalo@gmail.com>
2024-01-10 14:30:54 +0100

    Update ExternalTextures.java

--- src/main/java/net/fexcraft/mod/documents/ExternalTextures.java
+++ src/main/java/net/fexcraft/mod/documents/ExternalTextures.java
@@ -1,20 +1,28 @@
 package net.fexcraft.mod.documents;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.HttpTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.MultiPackResourceManager;
+import net.minecraft.server.packs.resources.ReloadableResourceManager;
+
+import java.io.File;
 import java.util.*;
 
 public class ExternalTextures {
 
 	private static final Map<String, ResourceLocation> MAP = new HashMap<String, ResourceLocation>();
 	private static final HashSet<String> KEY = new HashSet<>();
-	private static boolean added;
 	static{ KEY.add("documents"); }
 
 	public static ResourceLocation get(String url){
 		if(MAP.containsKey(url)) return MAP.get(url);
 		ResourceLocation texture = new ResourceLocation("documents", url.replaceAll("[^a-z0-9_.-]", ""));
 		MAP.put(url, texture);
-		//TODO
+		File file = new File("./temp/doc_download/" + texture.getPath());
+		if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+		file.deleteOnExit();
+		Minecraft.getInstance().textureManager.register(texture, new HttpTexture(file, url, texture, false, null));
 		return texture;
 	}
 
