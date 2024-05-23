42fff501168b365248345f4f4913f791df05af0f
Ferdinand Calo <fexcalo@gmail.com>
2024-05-23 22:44:47 +0200

    Update ClientEvents.java

--- src/main/java/net/fexcraft/mod/documents/ClientEvents.java
+++ src/main/java/net/fexcraft/mod/documents/ClientEvents.java
@@ -1,14 +1,10 @@
 package net.fexcraft.mod.documents;
 
-import com.mojang.blaze3d.platform.ScreenManager;
 import net.fexcraft.mod.documents.gui.DocEditorScreen;
 import net.fexcraft.mod.documents.gui.DocViewerScreen;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.world.inventory.MenuType;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
 import static net.neoforged.fml.common.Mod.EventBusSubscriber.Bus.*;
