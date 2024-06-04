7aa015ad8c3cc7dece39b4503f9170e1e10b0d02
Ferdinand Calo <fexcalo@gmail.com>
2024-06-05 00:01:42 +0200

    Update UiPacketReceiver.java

--- common/java/net/fexcraft/mod/documents/gui/UiPacketReceiver.java
+++ common/java/net/fexcraft/mod/documents/gui/UiPacketReceiver.java
@@ -1,22 +1,9 @@
 package net.fexcraft.mod.documents.gui;
 
-import net.fexcraft.mod.documents.packet.GuiPacketN;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import net.neoforged.neoforge.network.PacketDistributor;
 
 public interface UiPacketReceiver {
 
 	public void onPacket(CompoundTag com, boolean client);
-
-	public default void send(boolean toclient, CompoundTag compound, Player player){
-		if(toclient){
-			PacketDistributor.PLAYER.with((ServerPlayer)player).send(new GuiPacketN(compound));
-		}
-		else{
-			PacketDistributor.SERVER.noArg().send(new GuiPacketN(compound));
-		}
-	}
 
 }
