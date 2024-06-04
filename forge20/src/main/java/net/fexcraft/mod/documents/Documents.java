c2ae5526fc614ea9c051dd35ed44d35d6589d230
Ferdinand Calo <fexcalo@gmail.com>
2024-06-04 23:26:06 +0200

    Update Documents.java

--- forge20/src/main/java/net/fexcraft/mod/documents/Documents.java
+++ forge20/src/main/java/net/fexcraft/mod/documents/Documents.java
@@ -2,7 +2,13 @@ package net.fexcraft.mod.documents;
 
 import com.mojang.logging.LogUtils;
 import net.fexcraft.mod.documents.data.DocumentItem;
+import net.fexcraft.mod.documents.gui.UiPacketReceiver;
+import net.fexcraft.mod.documents.packet.GuiPacketF;
+import net.fexcraft.mod.documents.packet.SyncPacketF;
+import net.fexcraft.mod.fcl.util.ClientPacketPlayer;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.*;
 import net.minecraftforge.common.MinecraftForge;
@@ -14,6 +20,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.simple.SimpleChannel;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -24,7 +33,7 @@ import org.slf4j.Logger;
 @Mod(Documents.MODID)
 public class Documents {
 
-	public static final String MODID = "examplemod";
+	public static final String MODID = "documents";
 	private static final Logger LOGGER = LogUtils.getLogger();
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
@@ -32,6 +41,11 @@ public class Documents {
 	public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(Registries.MENU, MODID);
 	public static final RegistryObject<MenuType<DocEditorContainer>> DOC_EDITOR = CONTAINERS.register("editor", () -> IForgeMenuType.create(DocEditorContainer::new));
 	public static final RegistryObject<MenuType<DocViewerContainer>> DOC_VIEWER = CONTAINERS.register("viewer", () -> IForgeMenuType.create(DocViewerContainer::new));
+	public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(new ResourceLocation("documents", "channel"))
+		.clientAcceptedVersions(pro -> true)
+		.serverAcceptedVersions(pro -> true)
+		.networkProtocolVersion(() -> "documents")
+		.simpleChannel();
 
 	public Documents(){
 		IEventBus eventbus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -45,6 +59,28 @@ public class Documents {
 	private void commonSetup(final FMLCommonSetupEvent event){
 		DocRegistry.init();
 		DocPerms.loadperms();
+		CHANNEL.registerMessage(1, GuiPacketF.class, (packet, buffer) -> buffer.writeNbt(packet.com()), buffer -> new GuiPacketF(buffer.readNbt()), (packet, context) -> {
+			context.get().enqueueWork(() -> {
+				if(context.get().getDirection().getOriginationSide().isClient()){
+					ServerPlayer player = context.get().getSender();
+					((UiPacketReceiver)player.containerMenu).onPacket(packet.com(), false);
+				}
+				else{
+					((UiPacketReceiver)ClientPacketPlayer.get().containerMenu).onPacket(packet.com(), true);
+				}
+			});
+			context.get().setPacketHandled(true);
+		});
+		CHANNEL.registerMessage(2, SyncPacketF.class, (packet, buffer) -> packet.write(buffer), buffer -> SyncPacketF.read(buffer), (packet, context) -> {
+			context.get().enqueueWork(() -> {
+				if(context.get().getDirection().getOriginationSide().isClient()){
+					Documents.LOGGER.info(packet.map().toString());
+					DocRegistry.load(packet.map());
+					DocRegistry.DOCS.values().forEach(doc -> doc.linktextures());
+				}
+			});
+			context.get().setPacketHandled(true);
+		});
 	}
 
 	private void addCreative(BuildCreativeModeTabContentsEvent event){
@@ -59,7 +95,7 @@ public class Documents {
 		public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event){
 			if(event.getEntity().level().isClientSide) return;
 			DocRegistry.opj(event.getEntity());
-			//TODO PacketDistributor.PLAYER.with((ServerPlayer)event.getEntity()).send(new SyncPacket(DocRegistry.confmap));
+			CHANNEL.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer)event.getEntity()), new SyncPacketF(DocRegistry.confmap));
 		}
 
 		@SubscribeEvent
